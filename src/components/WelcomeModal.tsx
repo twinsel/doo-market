7481d3b0d38690ca35461e4f716a0908eef881b9
munzeralsmaeel <m0937324210@gmail.com>
2026-09-03 03:43:27 +0300

    Make welcome screen 100% solid on frame 1 to eliminate background flash

--- src/components/WelcomeModal.tsx
+++ src/components/WelcomeModal.tsx
@@ -73,11 +73,11 @@ export const WelcomeModal: React.FC = () => {
       {/* Welcome Overlay */}
       {showWelcome && (
         <motion.div
-          initial={{ opacity: 0 }}
+          initial={{ opacity: 1 }}
           animate={{ opacity: 1 }}
-          exit={{ opacity: 0 }}
+          exit={{ opacity: 0, transition: { duration: 0.5 } }}
           onClick={() => setShowWelcome(false)}
-          className="fixed inset-0 z-[9999] flex flex-col items-center justify-center bg-white p-6 text-center cursor-pointer select-none"
+          className="fixed inset-0 z-[99999] flex flex-col items-center justify-center bg-white p-6 text-center cursor-pointer select-none"
         >
           <motion.div
             initial={{ y: -50, opacity: 0 }}
